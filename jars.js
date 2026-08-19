affd07749a81a8fd5a6506b2f5f13b9333ebf095
Treemeantus <phewsionfood@gmail.com>
2026-08-19 18:46:40 +0530

    Show official raisins jar photo; keep working cashews jar

--- jars.js
+++ jars.js
@@ -1,16 +1,12 @@
 (function () {
-  function apply(cls, prefix, count) {
-    var urls = [];
-    for (var i = 1; i <= count; i++) urls.push(prefix + i + ".txt?v=8");
-    Promise.all(urls.map(function (u) { return fetch(u).then(function (r) { return r.text(); }); }))
-      .then(function (parts) {
-        var data = "url(data:image/jpeg;base64," + parts.join("") + ")";
-        document.querySelectorAll(cls).forEach(function (el) {
-          el.style.backgroundImage = data;
-        });
-      })
-      .catch(function () {});
-  }
-  apply(".img-cashews, .photo-cashews", "cashews", 6);
-  apply(".img-raisins, .photo-raisins", "raisins", 5);
+  var urls = [];
+  for (var i = 1; i <= 5; i++) urls.push("raisins" + i + ".txt?v=8");
+  Promise.all(urls.map(function (u) { return fetch(u).then(function (r) { return r.text(); }); }))
+    .then(function (parts) {
+      var data = "url(data:image/jpeg;base64," + parts.join("") + ")";
+      document.querySelectorAll(".img-raisins, .photo-raisins").forEach(function (el) {
+        el.style.backgroundImage = data;
+      });
+    })
+    .catch(function () {});
 })();
